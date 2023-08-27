08f9aa903cb53d3e9ae9f9f30771a33a545f5992
Dev Chand <chanddev123@gmail.com>
2023-08-27 20:30:47 +0545

    project init

---
+++ src/card/Card.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Pohara from "../assets/pokhara.jpg";
+import "./Card.css";
+
+const Card = ({ item }) => {
+  return (
+    <div className="card">
+      <div className="card-img">
+        <img src={Pohara} alt="" />
+      </div>
+
+      <div className="card-body">
+        <h4 className="card-title">{item}</h4>
+        <p className="para">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, nihil.
+          Sint delectus cumque blanditiis eligendi repudiandae optio! Molestias,
+          dolor voluptatibus facilis iste, dicta neque minus tenetur, quo totam
+          quis officia?
+        </p>
+        <button className="btn">Read More...</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
